a88582e8e1ada96b540fdc44a070ba9a9f64e0fd
napaeworn <73819215+napaeworn@users.noreply.github.com>
2021-09-20 14:09:55 +0700

    Update process.php

--- process.php
+++ process.php
@@ -1,11 +1,13 @@
 <?php
-  $username = $_GET['data1'];
-  $password = $_GET['data2'];
+  
+  $username =  $_GET['data1'];
+  $password =  $_GET['data2'];
 
   if($username == "admin" && $password == "1234"){
-    echo "welcome to your account"
+    echo "welcome to your account";
   }
-  else{
-    echo "Username or Password was incorrect"
+  else {
+    echo "Username or Password was incorrect";
   }
+
 ?>
