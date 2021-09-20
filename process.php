3670d0d2cfcf6c254fe31749e36932f7fa04d1da
napaeworn <73819215+napaeworn@users.noreply.github.com>
2021-09-20 14:42:12 +0700

    Update process.php

--- process.php
+++ process.php
@@ -3,27 +3,27 @@
   $d =  $_GET['score'];
 
   if($d >= 80){
-    echo "your grade is A";
+    echo "your grade is 4";
   }
   else if ($d >= 75){
-     echo "your grade is B+";
+     echo "your grade is 3.5";
   }
   else if ($d >= 70){
-    echo "your grade is B";
+    echo "your grade is 3";
   }
   else if ($d >= 65){
-     echo "your grade is C+";   
+     echo "your grade is 2.5";   
   }
   else if ($d >= 60){
-     echo "your grade is C";   
+     echo "your grade is 2";   
   }
   else if ($d >= 55){
-     echo "your grade is D+";   
+     echo "your grade is 1.5";   
   }
   else if ($d >= 50){
-     echo "your grade is D";   
+     echo "your grade is 1";   
   }
   else {
-     echo "sorry, you got grade F.";
+     echo "sorry, you got grade 0.";
   }
 ?>
