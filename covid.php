c62e53df579592dd7699da74748f004217fe9c37
napaeworn <73819215+napaeworn@users.noreply.github.com>
2021-11-26 16:17:44 +0700

    Create covid.php

---
+++ covid.php
@@ -0,0 +1,7 @@
+<?php
+  $json = file_get_contents('https://covid19.ddc.moph.go.th/api/Cases/today-cases-all%27);
+  $data = json_decode($json);
+  foreach ( $data[0] as $key => $val ){
+    echo $key.":".$val."<br>";
+  }
+?>
