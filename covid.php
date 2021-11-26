282903e5c52d624d14c04189ee32ab8ca1de9d21
napaeworn <73819215+napaeworn@users.noreply.github.com>
2021-11-26 16:28:32 +0700

    Update covid.php

--- covid.php
+++ covid.php
@@ -4,12 +4,10 @@
 
 
   echo "<table border='1'>";
-
-
-  foreach ( $data[0] as $key => $val ){
+   for($i=0;$i<8;$i++){
+          echo"<tr>foreach ( $data[0] as $key => $val ){
     echo $key.":".$val."<br>";
-  }
-
-
+  }</tr>";
+   
   echo "</table>";
 ?>
