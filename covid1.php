9af976e5e41e1c8562ca28801ec68d2ff770b5c8
napaeworn <73819215+napaeworn@users.noreply.github.com>
2021-12-03 13:59:54 +0700

    Update covid1.php

--- covid1.php
+++ covid1.php
@@ -1,19 +1,8 @@
 <?php
-  $json = file_get_contents("https://covid19.ddc.moph.go.th/api/Cases/today-cases-all");
-  $data = json_decode($json);
-
-  echo "<table border='1'>";
-
- foreach ( $data[0] as $key => $val ){
-    echo "<tr>";
-    echo "<td>";
-    echo $val."<br>"; 
-    echo "</td>";
-    echo "<td>";
-    echo $key;
-    echo "</td>";
-    echo "</tr>";
-  }
-
-  echo "</table>";
+ $json = file_get_contents("https://covid19.ddc.moph.go.th/api/Cases/today-cases-by-provinces");
+ $data = json_decode($json);
+ 
+ echo "<pre>";
+ var_dump($data);
+ echo "</pre>";
 ?>
