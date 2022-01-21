d2e259eb449babf77f9986d01afc9ccf74a3960d
napaeworn <73819215+napaeworn@users.noreply.github.com>
2022-01-21 14:17:47 +0700

    Update template.php

--- template.php
+++ template.php
@@ -13,15 +13,15 @@
         height: 160px;
         background-color: orange;
       }
-       #sidebar{
+      #sidebar{
         height: 450px;
         background-color: green;
       }
-       #content{
+      #content{
         height: 450px;
         background-color: gray;
       }
-       #footer{
+      #footer{
         height: 200px;
         background-color: pink;
       }
@@ -29,24 +29,25 @@
     
   </head>
   <body> 
-    <div class="container">
+    <div class="container"> 
       <div class="row">
-        <div class="col-12" id="header">
+        <div class="col-12" id="header"> 
           <h1>Header</h1>
-       </div>
+        </div>
       </div>
       <div class="row">
-       <div class="col-3" id ="sidebar">
-         <h1>Sidebar</h1>
+        <div class="col-3" id="sidebar"> 
+          <h1>Sidebar</h1>
+        </div>
+        <div class="col-9" id="content"> 
+          <h1>Content</h1>
+        </div>
       </div>
-      <div class ="col-9" id="content">
-         <h1>Content</h1>
-       </div>
+       <div class="row">
+        <div class="col-12" id="footer"> 
+          <h1>Footer</h1>
+        </div>
       </div>
-      <div class = "col-12" id="footer">
-         <h1>Footer</h1>
-      </div>
-     </div>   
     </div>  
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
