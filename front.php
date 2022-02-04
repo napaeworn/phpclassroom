78bcc1920bdc307c6c693694d29b59d0834fc48d
napaeworn <73819215+napaeworn@users.noreply.github.com>
2022-02-04 15:31:57 +0700

    Update front.php

--- front.php
+++ front.php
@@ -2,16 +2,28 @@
   <head>
     <title>การกำหนดรูปแบบตัวอักษร</title>
     <style>
-     @font-face{
-       font-family : myFont;
-       src : url(duck b.woff);
-     }
-      body{
-       font-family: myFont;
-     }
+      @font-face{
+        font-family: myFont;
+        src: url(duck b.woff);
+      }
+      @font-face{
+        font-family: myFont2;
+        src: url(.....);
+      }
+      #font1{
+        font-family: myFont;
+      }
+      #font2{
+        font-family: myFont2;
+      }
     </style>
   </head>
   <body>
-    <h1>เว็บไซต์ของฉัน</h1>
+    <div id="font1">
+      <h1>นายณปวร แก้วโชติ เลขที่ 2 ห้อง 5/13</h1>
+    </div>
+    <div id="font2">
+      <h1>นายณปวร แก้วโชติ เลขที่ 2 ห้อง 5/13</h1>
+    </div>
   </body>
 </html>
