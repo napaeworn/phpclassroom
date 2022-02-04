42dc5a9c04189fae16ccb25c59d6bd3133898536
napaeworn <73819215+napaeworn@users.noreply.github.com>
2022-02-04 15:25:07 +0700

    Update front.php

--- front.php
+++ front.php
@@ -1,6 +1,15 @@
 <html>
   <head>
     <title>การกำหนดรูปแบบตัวอักษร</title>
+    <style>
+     @front-face{
+       front-family : "myfront";
+       src : url(duck b.woff)
+     }
+      body{
+         front-family: "myfront";
+      }
+    </style>
   </head>
   <body>
     <h1>เว็บไซต์ของฉัน</h1>
