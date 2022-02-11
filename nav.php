36d2293f52949126f494883d79f4845b3bfa1a0c
napaeworn <73819215+napaeworn@users.noreply.github.com>
2022-02-11 14:04:39 +0700

    Update nav.php

--- nav.php
+++ nav.php
@@ -1,12 +1,27 @@
 <html>
   <head>
     <title>Navigation Bar</title>
+    <style>
+      ul {
+        list-style-type:none;
+      }
+      li a {
+        display : block;
+        padding: 8px 16px;
+        text- decoration:none;
+        background-color: green;
+      }
+      li a:hover {
+        background-color: #555;
+        color: white;
+      }
+    </style>
   </head>
   <body>
      <ul>
-      <li>Coffee</li> 
-      <li>Tea</li> 
-      <li>Milk</li> 
+      <li><a href = "#">Coffee</li> 
+      <li><a href = "#">Tea</li> 
+      <li><a href = "#">Milk</li> 
     </ul>
   </body>
 </html>
