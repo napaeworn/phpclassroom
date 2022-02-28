fac54dcd531d59c2cc5ace211a60c4ffdea3c53e
napaeworn <73819215+napaeworn@users.noreply.github.com>
2022-02-28 10:27:15 +0700

    Update template.php

--- template.php
+++ template.php
@@ -32,7 +32,7 @@
     <div class="container"> 
       <div class="row">
         <div class="col-12" id="header">
-          <h1>Hจังหวัดเชียงใหม่</h1>
+          <h1>จังหวัดเชียงใหม่</h1>
         </div>
       </div>
       <div class="row">
