9358d7c5e9a998646ff125927d342b7f00709598
napaeworn <73819215+napaeworn@users.noreply.github.com>
2022-03-02 17:23:38 +0700

    Update lswproject.php

--- lswproject.php
+++ lswproject.php
@@ -19,7 +19,7 @@
       #header{
         height: 160px;
         background-image:url("p.jpg");
-        background-size: 20%;
+        background-size: 40%;
       }
       #sidebar{
         height: 450px;
