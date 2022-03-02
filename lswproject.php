5fb1d4ea3f6c3792095c4404bf5e977b87a1ef87
napaeworn <73819215+napaeworn@users.noreply.github.com>
2022-03-02 21:56:33 +0700

    Update lswproject.php

--- lswproject.php
+++ lswproject.php
@@ -87,7 +87,7 @@
           <h4>สัญลักษณ์ประจำจังหวัด</h4><br>
           <center>
             <h5>• ต้นไม้   ทองกวาว</h5><br>
-            <img src="tong.jpg" width = "300px" class="img-fluid rounded"><br>
+            <img src="tong.jpg" width = "300px" class="img-fluid rounded"><br><br>
             
             <h5>• ดอกไม้  ทองกวาว</h5><br>
             
