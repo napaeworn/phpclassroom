a16ae714c701e186604a6abc7080b066e698c9bf
napaeworn <73819215+napaeworn@users.noreply.github.com>
2022-03-02 17:08:59 +0700

    Update lswproject.php

--- lswproject.php
+++ lswproject.php
@@ -7,11 +7,19 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>chiangmai province website</title>
-    
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css?family=Sarabun"
+    />
     <style>
+      body {
+        font-family : "sarabun";
+        background-color : #CCFFFF ;
+      }
       #header{
         height: 160px;
-        background-color: orange;
+        background-image:url(download.png);
+        background-size: 100 %;
       }
       #sidebar{
         height: 450px;
