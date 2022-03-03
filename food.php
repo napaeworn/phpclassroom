2ff65ab6d1eb63adcccac504c1a3ca14de7b3a7c
napaeworn <73819215+napaeworn@users.noreply.github.com>
2022-03-03 12:43:30 +0700

    Update food.php

--- food.php
+++ food.php
@@ -5,11 +5,15 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css
+
+" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>chiangmai province website</title>
     <link
       rel="stylesheet"
-      href="https://fonts.googleapis.com/css?family=Sarabun"
+      href="https://fonts.googleapis.com/css?family=Sarabun
+
+"
     />
     <style>
       body {
@@ -18,6 +22,7 @@
       }
       #header{
         height: 160px;
+        padding: 50px;
         background-image: url("p.jpg");
         background-size:100%;
       }
@@ -26,7 +31,6 @@
         padding: 0px;
       }
       #content{
-        height: 450px;
         padding: 10px;
         background-color: gray;
       }
@@ -64,7 +68,7 @@
     <div class="container"> 
       <div class="row">
         <div class="col-12" id="header">
-          <h1>จังหวัดเชียงใหม่</h1>
+          <center><h1>จังหวัดเชียงใหม่</h1></center>
         </div>
       </div>
       <div class="row">
@@ -78,8 +82,37 @@
           </ul>
         </div>
         <div class="col-9" id="content"> 
-          <h1>รอน้องปูนทำงานก่อนนะครับ</h1>
-          <center><h1>cumming(come) soon </h1></center>
+          <h4>อาหารพื้นเมืองเชียงใหม่</h4><br>
+          
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"เชียงใหม่" นอกจากจะมีสถานที่ท่องเที่ยวสวยงามมากมายให้เที่ยวได้แบบไม่ซ้ำแล้ว อาหารการกินก็อุดมสมบูรณ์ไม่แพ้กัน วันนี้ทางเราจะขอพาไปลำกับอาหารพื้นเมืองเชียงใหม่แบบชาวเหนือแต๊ ๆ กับอาหารเหนือ 7 อย่างที่ไม่ควรพลาดถ้าไปเยือนเชียงใหม่ <br>
+        
+         
+           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>•ข้าวซอย</b> ถ้าไม่ได้กินเมนูโด่งดังอย่างข้าวซอย ก็คงพูดไม่ได้เต็มปากว่ามาถึงเชียงใหม่ ข้าวซอยถือเป็นอาหารเมืองเหนือที่หลาย ๆ คนหลงรัก มาเชียงใหม่ทีไรก็ต้องหาร้านข้าวซอยกินตลอด ด้วยความนุ่มของเนื้อ ความหนึบของเส้นข้าวซอย และความหวานมันของน้ำแกง ทำให้ใคร ๆ ก็ต้องหลงรักเมนูนี้แบบเทใจให้<br>
+          <center>  <img src="ข้าวซอย.jpg" width = "360px" class="img-fluid rounded"><br>
+            
+            
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>•น้ำเงี้ยว</b> นึกถึงเมนูอาหารเหนือที่เชียงใหม่ก็ห้ามพลาดเมนูน้ำเงี้ยวอย่างแน่นอน โดยเฉพาะเมนูขนมจีนน้ำเงี้ยวที่ใครต่อใครได้ทานก็ตกหลุมรักไปตาม ๆ กัน เพราะในน้ำเงี้ยวมีทั้งเนื้อหมูนุ่ม ๆ เลือดไก่ต้มพูน ๆ น้ำแกงข้น ๆ และที่ขาดไม่ได้ก็คือดอกงิ้ว แค่จานเดียวก็ได้รสสัมผัสถึงกลิ่นไออาหารเหนือแท้ ๆ<br>
+          <center>  <img src="น้ำเงี้ยว.jpg" width = "360px" class="img-fluid rounded"><br>
+           
+         
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>•น้ำพริกอ่อง</b> น้ำพริกอ่องเป็นน้ำพริกพื้นบ้านของภาคเหนือ ลักษณะเด่นของน้ำพริกอ่อง คือ จะมีสีส้มของสีมะเขือเทศและพริกแห้ง ที่เคี่ยวจนเป็นน้ำขลุกขลิก มีน้ำมันลอยหน้าเล็กน้อย นิยมรับประทานกับผักสดหรือผักต้มก็ได้ และที่นิยมรับประทานร่วมกันอีกอย่างหนึ่งคือ แคบหมู ซึ่งจะทำให้ได้อรรถรสในการกินเพิ่มมากขึ้น<br>
+          <center> <img src="น้ำพริกอ่อง.png" width = "360px" class="img-fluid rounded"><br>
+           
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>•น้ำพริกหนุ่ม</b> ถ้าจะซื้อของฝากจากเชียงใหม่ก็คงขาดไม่ได้กับเมนูน้ำพริกหนุ่ม หนึ่งในอาหารพื้นบ้านชื่อดังของทางเหนือที่เปรียบเสมือนเป็นตัวแทนของชาวเชียงใหม่ เกิดจากการนำเอา "พริกหนุ่ม" มาย่างไฟจนได้ที่ แล้วจึงนำมาตำรวมกับเครื่องเคราหลากชนิด อาทิ หอมแดง กระเทียม ปลาแห้ง ปลาร้า เกลือ เป็นต้น<br>
+          <center> <img src="น้ำพริกหนุ่ม.jpg" width = "360px" class="img-fluid rounded"><br>
+          
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>•ไส้อั่ว</b> หนึ่งในอาหารพื้นเมืองและของฝากยอดนิยมทางภาคเหนือของประเทศไทย "อั่ว" หมายถึง กรอก หรือ ยัด ปกตินิยมใช้เนื้อหมู มันหมู นำมาปรุงรส ด้วยเครื่องเทศ โขลกละเอียด คลุกให้เข้ากัน แล้วกรอกลงไปในไส้อ่อนของหมูที่เกลาจนบางแล้ว บิดให้เป็นท่อนพอประมาณ จากนั้นนำไปย่างให้เกรียม จะทำให้มีกลิ่นหอมชวนรับประทานที่สุด<br>
+          <center> <img src="ไส่อั่ว.jpg" width = "360px" class="img-fluid rounded"><br>
+            
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>•แกงฮังเล </b> อาหารขึ้นชื่อของชาวเหนือล้านนา สุดยอดเมนูเด็ด แกงฮังเล หรือ แกงฮินเล เป็นอาหารที่มีรสชาติเค็ม-เปรี้ยว มีต้นกำเนิดมาจากประเทศพม่า ชาวไทยภาคเหนือและแคว้นสิบสองปันนา ประเทศจีนนิยมรับประทานกัน มาเที่ยวภาคเหนือต้องได้กินสักครั้ง<br>
+          <center> <img src="แก.jpg" width = "360px" class="img-fluid rounded"><br>
+            
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>•ลาบหมูคั่ว</b> ลาบเป็นอาหารพื้นบ้านอีกอย่างหนึ่งของคนเหนือ เป็นอาหารที่นิยมทำกินกันในโอกาสพิเศษหรืองานเลี้ยงหรือในเทศกาลต่าง ๆ ส่วนประกอบหลักของลาบหมู คือเนื้อหมูสด และหัวใจสำคัญที่สุดคือ "พริกลาบ" ที่ส่งกลิ่นหอมจากเครื่องเทศหลากชนิด หรือจะเป็นลาบสุกที่เสิรมรสเผ็ดร้อนได้อย่างถูกลิ้นชาวเหนือ <br>
+          <center> <img src="ลาบหมูคั่ว.jpg" width = "360px" class="img-fluid rounded"><br>
+           
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>•eieioyae</b> 
+           <center>
+            
         </div>
       </div>
        <div class="row">
@@ -94,6 +127,8 @@
       </div>
     </div>  
     <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js
+
+" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
   </body>
 </html>
