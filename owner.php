582eaff2b7088e27a9ae6d1b85efa7939f2ca950
napaeworn <73819215+napaeworn@users.noreply.github.com>
2022-03-03 09:36:16 +0700

    Update owner.php

--- owner.php
+++ owner.php
@@ -79,7 +79,7 @@
         </div>
         <div class="col-9" id="content"> 
           <b><h4>ข้อมูลของผู้จัดทำ</h4></b>
-          
+          <img src="poon.jpg" width = "650px"stlye="foat:left" class="img-fluid rounded"><br><br>
         </div>
       </div>
        <div class="row">
